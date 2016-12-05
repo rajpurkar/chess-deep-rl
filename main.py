f596dc93dff9ef384e1d6946ce81eb362bad4d5b
Toki Migimatsu <toki.migimatsu@gmail.com>
2016-12-05 02:50:52 -0800

    updating main

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ d = Dataset("data/small.pgn")
 # d = Dataset("data/medium.pgn")
 # d = Dataset("data/CCRL-4040.[671444].pgn")
 # d = Dataset("data/ficsgamesdb_1999-2015_standard2000_nomovetimes.pgn")
-for state, action, result in d.random_states():
-    print(state.shape, action, result)
+
+for state, reward in d.random_black_state():
+    print(state.shape, reward)
