73acdc9b3fa706f559d39ed66324963e098beae4
tmigimatsu <toki.migimatsu@gmail.com>
2016-12-05 22:29:45 -0800

    save intermediate progress on Q

--- q_learning.py
+++ q_learning.py
@@ -7,10 +7,15 @@ import time
 dim_S = data.NUM_SQUARES * data.NUM_PIECES * data.NUM_COLORS
 dim_A = dim_S * dim_S
 
+filename = "ficsgamesdb_1999-2015_standard2000_nomovetimes"
+datafile = "data/%s.pgn" % filename
+picklefile = "engines/sarsa_Q_%s-%s.pickle" % (filename, time.time())
+
 def sarsa_lambda(data, LAMBDA=0.99, GAMMA=0.99, ALPHA=1/np.sqrt(dim_S)):
     Q = {}
     N = {}
     try:
+        i = 0
         for s, a, r, s_prime, a_prime in tqdm(data.white_sarsa()):
             if s not in N:
                 Q[s] = {}
@@ -28,13 +33,20 @@ def sarsa_lambda(data, LAMBDA=0.99, GAMMA=0.99, ALPHA=1/np.sqrt(dim_S)):
                 for a_hat in N[s_hat]:
                     Q[s_hat][a_hat] += ALPHA * delta * N[s_hat][a_hat]
                     N[s_hat][a_hat] *= GAMMA * LAMBDA
+
+            if i % 25000 == 0:
+                with open(picklefile, "wb") as f:
+                    pickle.dump(Q, f)
+            i += 1
     except KeyboardInterrupt:
         return Q, N
     return Q, N
 
-filename = "medium"
-d = data.Dataset("data/%s.pgn" % filename)
-Q, N = sarsa_lambda(d)
+try:
+    d = data.Dataset(datafile)
+    Q, N = sarsa_lambda(d)
+except Exception as e:
+    print(e)
 
-with open("engines/sarsa_Q_%s-%s.pickle" % (filename, time.time()), "wb") as f:
+with open(picklefile, "wb") as f:
     pickle.dump(Q, f)
