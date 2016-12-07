81ba0d3eb784d8a3a94d1af9bd27aa9b5002eacf
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2016-12-07 14:40:41 -0800

    Update model to use state featurized

--- model.py
+++ model.py
@@ -17,8 +17,7 @@ def common_network(**kwargs):
     from keras.layers.normalization import BatchNormalization
     from keras.layers.advanced_activations import PReLU
     defaults = {
-        "board": 8,
-        "board_depth": 12,
+        "board_side_length": 8,
         "layers": 4,
         "num_filters": 6,
         "dropout": 0
@@ -27,9 +26,9 @@ def common_network(**kwargs):
     params.update(kwargs)
 
     conv_input = Input(shape=(
-        params["board_depth"],
-        params["board"],
-        params["board"]))
+        params["board_num_channels"],
+        params["board_side_length"],
+        params["board_side_length"]))
 
     conv_mess = conv_input
     for i in range(0, params["layers"]):
@@ -49,18 +48,6 @@ def common_network(**kwargs):
             conv_mess = Dropout(params["dropout"])(conv_mess)
     flattened = Flatten()(conv_mess)
     return conv_input, flattened
-
-
-def value_network(**kwargs):
-    from keras.models import Model
-    from keras.layers import Dense
-    """ Use a variation of the ROCAlphaGo Value Network. """
-    conv_input, flattened = common_network(**kwargs)
-    dense_1 = Dense(500, activation="relu")(flattened)
-    output = Dense(1, activation="tanh")(dense_1)
-    model = Model(conv_input, output)
-    model.compile('adamax', 'mse')
-    return model
 
 
 def policy_network(**kwargs):
@@ -105,29 +92,24 @@ def get_filename_for_saving(net_type, start_time):
 
 
 def train(net_type):
-    from keras.callbacks import ModelCheckpoint
     d = Dataset('data/large-ccrl.pgn')
     start_time = str(int(time.time()))
+    generator_fn = d.white_state_action_sl
+    d_test = Dataset('data/small_test.pgn')
+    X_val, y_from_val, y_to_val = d_test.load('white_state_action_sl', refresh=True)
+    print(X_val[0].shape)
+    model = policy_network(board_num_channels=X_val[0].shape[0])
+    from keras.callbacks import ModelCheckpoint
     checkpointer = ModelCheckpoint(
-        filepath=get_filename_for_saving(net_type, start_time),
+        filepath=get_filename_for_saving('policy', start_time),
         verbose=2,
         save_best_only=True)
-    if net_type == "value":
-        model = value_network()
-        generator_str = 'random_black_state'
-        generator_fn = d.random_black_state
-    elif net_type == 'policy':
-        model = policy_network()
-        generator_str = 'white_state_action_sl'
-        generator_fn = d.white_state_action_sl
-    d_test = Dataset('data/medium_test.pgn')
-    X_test, y_start, y_end = d_test.load(generator_str, refresh=False)
     model.fit_generator(
-        generator_fn(),
+        generator_fn(featurized=True),
         samples_per_epoch=SAMPLES_PER_EPOCH,
         nb_epoch=NUMBER_EPOCHS,
         callbacks=[checkpointer],
-        validation_data=(X_test, [y_start, y_end]),
+        validation_data=(X_val, [y_from_val, y_to_val]),
         verbose=VERBOSE_LEVEL)
 
 if __name__ == '__main__':
