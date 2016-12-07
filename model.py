6f492ef6b9d46c67c4bffcbbd7c41187e0b905a7
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2016-12-06 22:25:29 -0800

    Add parse network type option + refactor imports

--- model.py
+++ model.py
@@ -1,10 +1,6 @@
-from keras.callbacks import ModelCheckpoint
-from keras.layers.convolutional import Convolution2D
-from keras.layers import Input, Dense, Flatten
-from keras.models import Model
 import time
 import os
-# from tqdm import tqdm
+import argparse
 from data import Dataset
 
 FOLDER_TO_SAVE = "./saved/"
@@ -14,9 +10,11 @@ VERBOSE_LEVEL = 1
 
 
 def common_network(**kwargs):
+    from keras.layers.convolutional import Convolution2D
+    from keras.layers import Input, Flatten
     defaults = {
         "board": 8,
-        "board_depth": 29,
+        "board_depth": 12,
         "layers": 5,
         "num_filters": 100
     }
@@ -32,7 +30,7 @@ def common_network(**kwargs):
     for i in range(0, params["layers"]):
         # use filter_width_K if it is there, otherwise use 3
         filter_key = "filter_width_%d" % i
-        filter_width = params.get(filter_key, 11 - i*2)
+        filter_width = params.get(filter_key, 1 + i*2)
         conv_start = Convolution2D(
             nb_filter=params["num_filters"],
             nb_row=filter_width,
@@ -55,21 +53,25 @@ def common_network(**kwargs):
 
 
 def value_network(**kwargs):
+    from keras.models import Model
+    from keras.layers import Dense
     """ Use a variation of the ROCAlphaGo Value Network. """
     conv_input, flattened = common_network(**kwargs)
-    dense_1 = Dense(1000, activation="relu")(flattened)
-    dense_2 = Dense(500, activation="relu")(dense_1)
-    dense_3 = Dense(250, activation="relu")(dense_2)
-    output = Dense(6, activation="softmax")(dense_3)
+    dense_1 = Dense(500, activation="relu")(flattened)
+    output = Dense(1, activation="tanh")(dense_1)
     model = Model(conv_input, output)
     model.compile('adamax', 'mse')
     return model
 
 
-def six_piece_policy_network(**kwargs):
-    conv_input, flattened = common_network(**kwargs) 
-    dense_1 = Dense(250, activation="relu")(flattened)
-    output = Dense(6, activation="softmax")(dense_1)
+def policy_network(**kwargs):
+    from keras.models import Model
+    from keras.layers import Dense
+    conv_input, flattened = common_network(**kwargs)
+    dense_1 = Dense(1000, activation="relu")(flattened)
+    dense_2 = Dense(500, activation="relu")(dense_1)
+    dense_3 = Dense(250, activation="relu")(dense_2)
+    output = Dense(64, activation="softmax")(dense_3)
     model = Model(conv_input, output)
     model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
     return model
@@ -83,6 +85,7 @@ def get_filename_for_saving(net_type, start_time):
 
 
 def train(net_type):
+    from keras.callbacks import ModelCheckpoint
     d = Dataset('data/medium.pgn')
     start_time = str(int(time.time()))
     checkpointer = ModelCheckpoint(
@@ -93,12 +96,12 @@ def train(net_type):
         model = value_network()
         generator_str = 'random_black_state'
         generator_fn = d.random_black_state
-    elif net_type == 'six_piece_policy':
-        model = six_piece_policy_network()
+    elif net_type == 'policy':
+        model = policy_network()
         generator_str = 'white_state_action_sl'
         generator_fn = d.white_state_action_sl
     d_test = Dataset('data/medium_test.pgn')
-    (X_test, y_test) = d_test.load(generator_str)
+    (X_test, y_test) = d_test.load(generator_str, refresh=False)
     model.fit_generator(
         generator_fn(),
         samples_per_epoch=SAMPLES_PER_EPOCH,
@@ -108,4 +111,7 @@ def train(net_type):
         verbose=VERBOSE_LEVEL)
 
 if __name__ == '__main__':
-    train('six_piece_policy')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("network_type", help="Either value or policy")
+    args = parser.parse_args()
+    train(args.network_type)
