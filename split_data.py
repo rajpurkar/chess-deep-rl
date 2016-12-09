43a6e4f35ae8cf945ba5ced41badd68caf8983b0
Toki Migimatsu <toki.migimatsu@gmail.com>
2016-12-09 10:15:09 -0800

    split data at intervals

--- split_data.py
+++ split_data.py
@@ -1,23 +1,33 @@
 import os
 
-filename = "small.pgn"
-size_test = 500
+filename = "data/large-ccrl.pgn"
+size_test = 200
 
 name, ext = os.path.splitext(filename)
 train_filename = name + "_train" + ext
 test_filename = name + "_test" + ext
 
 with open(filename) as f:
-    with open(test_filename, "w") as f_test:
-        idx_game = 0
-        for line in f:
-            if not line.strip():
-                idx_game += 1
-            f_test.write(line)
-            if idx_game == size_test * 2:
-                f_test.write("\n")
-                break
-
     with open(train_filename, "w") as f_train:
+        with open(test_filename, "w") as f_test:
+            idx_game = 0
+            for line in f:
+                if not line.strip():
+                    idx_game += 1
+
+                    if idx_game % 2 == 0:
+                        idx_game_train = 0
+                        for line in f:
+                            if not line.strip():
+                                idx_game_train += 1
+                            f_train.write(line)
+                            if idx_game_train == 1000:
+                                break
+
+                f_test.write(line)
+                if idx_game == size_test * 2:
+                    f_test.write("\n")
+                    break
+
         for line in f:
             f_train.write(line)
