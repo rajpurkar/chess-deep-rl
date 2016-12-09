776db875a3bfff1901a4986f276b2e96c213a181
Toki Migimatsu <toki.migimatsu@gmail.com>
2016-12-08 19:24:31 -0800

    fix uci communication

--- engines/QEngine.py
+++ engines/QEngine.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python3
-from ChessEngine import ChessEngine
-import chess
 import sys
 sys.path.append('.')
+from ChessEngine import ChessEngine
 import data
+import chess
+import pickle
+import random
 
 class QEngine(ChessEngine):
     def __init__(self, picklefile):
         super().__init__()
         with open(picklefile, "rb") as f:
-            self.Q = pickle.load(Q, f)
+            self.Q = pickle.load(f)
 
     def search(self):
-        s = data.state_from_board(board, hashable=True)
+        s = data.state_from_board(self.board, hashable=True)
         try:
-            a = Q[s]
-            from_square = a // NUM_SQUARES
-            to_square = a % NUM_SQUARES
+            a = self.Q[s]
+            from_square = a // data.NUM_SQUARES
+            to_square = a % data.NUM_SQUARES
             move = chess.Move(from_square, to_square)
+            if not self.board.is_legal(move):
+                raise Exception()
         except:
             moves = list(self.board.generate_legal_moves())
+            if not moves:
+                self.moves = None
+                return
             move = random.choice(moves)
         self.moves = [move]
 
 if __name__ == "__main__":
-    engine = QEngine("engines/sarsa_Q_-_.pickle")
+    engine = QEngine("engines/sarsa_Q_CCRL.pickle")
     engine.run()
