1992139c30409c3cadff368963dee6e98b23219d
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2016-12-10 13:29:49 -0800

    Update to use same architecture as policy network

--- value_model.py
+++ value_model.py
@@ -7,20 +7,26 @@ np.random.seed(20)
 
 FOLDER_TO_SAVE = "./saved/"
 NUMBER_EPOCHS = 10000  # some large number
-SAMPLES_PER_EPOCH = 10016  # tune for feedback/speed balance
+SAMPLES_PER_EPOCH = 12800  # tune for feedback/speed balance
 VERBOSE_LEVEL = 1
 
-
-def common_network(**kwargs):
+def build_network(**kwargs):
+    from keras.models import Model
     from keras.layers.convolutional import Convolution2D
-    from keras.layers import Input, Flatten, Dropout
     from keras.layers.normalization import BatchNormalization
     from keras.layers.advanced_activations import PReLU
+    from keras.layers import Dense, Dropout, Activation, \
+        Reshape, Flatten, Input, merge
+
     defaults = {
         "board_side_length": 8,
-        "layers": 3,
-        "num_filters": 128,
-        "dropout": 0.3
+        "conv_layers": 4,
+        "num_filters": 32,
+        "dropout": 0.3,
+        "dense_layers": 2,
+        "dense_hidden": 64,
+        "output_size": 1,
+        "conditioned_architecture": True
     }
     params = defaults
     params.update(kwargs)
@@ -30,69 +36,46 @@ def common_network(**kwargs):
         params["board_side_length"],
         params["board_side_length"]))
 
-    conv_mess = conv_input
-    for i in range(0, params["layers"]):
+    def conv_wrap(conv_out):
         # use filter_width_K if it is there, otherwise use 3
         filter_key = "filter_width_%d" % i
         filter_width = params.get(filter_key, 3)
         num_filters = params["num_filters"]
-        conv_mess = Convolution2D(
+        conv_out = Convolution2D(
             nb_filter=num_filters,
             nb_row=filter_width,
             nb_col=filter_width,
             init='he_normal',
-            border_mode='same')(conv_mess)
-        conv_mess = BatchNormalization()(conv_mess)
-        conv_mess = PReLU()(conv_mess)
+            border_mode='same')(conv_out)
+        conv_out = BatchNormalization()(conv_out)
+        conv_out = PReLU()(conv_out)
         if params["dropout"] > 0:
-            conv_mess = Dropout(params["dropout"])(conv_mess)
-    flattened = Flatten()(conv_mess)
-    return conv_input, flattened
+            conv_out = Dropout(params["dropout"])(conv_out)
+        return conv_out
 
-def value_network(**kwargs):
-    from keras.models import Model
-    from keras.layers import Dense
-    """ Use a variation of the ROCAlphaGo Value Network. """
-    conv_input, flattened = common_network(**kwargs)
-    dense_1 = Dense(128, activation="relu")(flattened)
-    dense_2 = Dense(128, activation="relu")(dense_1)
-    output = Dense(1, activation="tanh")(dense_2)
-    model = Model(conv_input, output)
-    model.compile('adamax', 'mse')
-    return model
+    def dense_wrap(dense_out):
+        dense_out = Dense(params["dense_hidden"],
+                          init="he_normal")(dense_out)
+        dense_out = BatchNormalization()(dense_out)
+        dense_out = PReLU()(dense_out)
+        if params["dropout"] > 0:
+            dense_out = Dropout(params["dropout"])(dense_out)
+        return dense_out
 
-def policy_network(**kwargs):
-    from keras.models import Model
-    from keras.layers import Dense, Dropout, merge
-    from keras.layers.normalization import BatchNormalization
-    from keras.layers.advanced_activations import PReLU
+    conv_out = conv_input
+    for i in range(0, params["conv_layers"]):
+        conv_out = conv_wrap(conv_out)
 
-    params = {
-        "dense_layers": 1,
-        "dense_hidden": 64,
-        "output_size": 64,
-        "dropout": 0
-    }
-    params.update(kwargs)
+    flattened = Flatten()(conv_out)
 
-    conv_input, flattened = common_network(**kwargs)
-    dense_mess = flattened
+    dense_out = flattened
     for i in range(params["dense_layers"]):
-        dense_mess = Dense(params["dense_hidden"], init="he_normal")(dense_mess)
-        dense_mess = BatchNormalization()(dense_mess)
-        dense_mess = PReLU()(dense_mess)
-        if params["dropout"] > 0:
-            dense_mess = Dropout(params["dropout"])(dense_mess)
+        dense_out = dense_wrap(dense_out)
+    value = Dense(params["output_size"], activation="tanh", name='value')(dense_out)
 
-    # output for the first board
-    output_from = Dense(params["output_size"], activation="softmax")(dense_mess)
-    merged_output_from = merge([output_from, dense_mess], mode='concat')
+    model = Model(conv_input, value)
 
-    # output for the second board
-    output_to = Dense(params["output_size"], activation="softmax")(merged_output_from)
-
-    model = Model(conv_input, [output_from, output_to])
-    model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
+    model.compile('adamax', 'mse', metrics=['mean_squared_error'])
     return model
 
 def plot_model(model, net_type, start_time):
@@ -113,7 +96,7 @@ def get_filename_for_saving(net_type, start_time):
     return folder_name + "/{epoch:02d}-{val_loss:.2f}.hdf5"
 
 
-def train(net_type):
+def train():
     featurized = False
 
     d = Dataset('data/large-ccrl_train.pgn')
@@ -124,16 +107,13 @@ def train(net_type):
         featurized = featurized,
         refresh    = False)
 
-    if net_type == "value":
-        model = value_network(board_num_channels=X_val[0].shape[0])
-    else:
-        model = policy_network(board_num_channels=X_val[0].shape[0])
+    model = build_network(board_num_channels=X_val[0].shape[0])
     start_time = str(int(time.time()))
-    plot_model(model, net_type, start_time)
+    plot_model(model, 'value', start_time)
 
     from keras.callbacks import ModelCheckpoint
     checkpointer = ModelCheckpoint(
-        filepath       = get_filename_for_saving(net_type, start_time),
+        filepath       = get_filename_for_saving('value', start_time),
         verbose        = 2,
         save_best_only = True)
 
@@ -145,7 +125,4 @@ def train(net_type):
         verbose           = VERBOSE_LEVEL)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("network_type", help="Either value or policy")
-    args = parser.parse_args()
-    train(args.network_type)
+    train()
